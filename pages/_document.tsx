e4f05875a5d89161311a51306b281588538712bf
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-06-18 02:46:28 +0200

    Added lang attribute to the HTML in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class _Document extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             rel="apple-touch-icon"
