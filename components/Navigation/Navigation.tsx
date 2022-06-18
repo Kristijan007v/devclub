dabb91711244a167029b667e90744462c0bc6518
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-06-18 02:25:58 +0200

    Downgraded to Next.js 12.1.5 because of FOUC, properly configured Next.js _document.tsx to work with Mantine

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import { Kbd, Text, Anchor } from "@mantine/core";
 import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 import SearchData from "../Search/SearchData";
+import Link from "next/link";
 
 interface Props {
   autoComplete: string[];
@@ -11,18 +12,20 @@ export default function Navigation({ autoComplete }: Props) {
     <nav className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-2 justify-between items-center p-4 w-full">
       <div className="flex items-center justify-between w-full">
         {/* <a className="font-extrabold text-3xl ">DEVCLUB</a> */}
-        <Anchor
-          sx={(theme) => ({
-            color:
-              theme.colorScheme === "dark"
-                ? theme.colors.gray[5]
-                : theme.colors.dark[6],
-          })}
-          href="/"
-          className="font-extrabold text-3xl "
-        >
-          DEVCLUB
-        </Anchor>
+        <Link href="/">
+          <Anchor
+            component="a"
+            sx={(theme) => ({
+              color:
+                theme.colorScheme === "dark"
+                  ? theme.colors.gray[5]
+                  : theme.colors.dark[6],
+            })}
+            className="font-extrabold text-3xl "
+          >
+            DEVCLUB
+          </Anchor>
+        </Link>
         <div className="block md:hidden">
           <ThemeSwitch />
         </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,40 +1,46 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      {/* Add whichever language you want here */}
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link
-          rel="favicon"
-          type="image/png"
-          sizes="512x512"
-          href="/favicon.ico"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1a1b1e" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link
+            rel="favicon"
+            type="image/png"
+            sizes="512x512"
+            href="/favicon.ico"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#1a1b1e" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
