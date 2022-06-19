188dbac927bfb39c8f32fdb2ef310b3f5d8148b3
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-06-19 16:19:53 +0200

    Fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
+export const config = {
   siteUrl: process.env.SITE_URL || "https://www.devclub.blog/",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
