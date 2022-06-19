5d81fcd23ea86ef31e0fc43f0a366d2f141004c9
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-06-19 16:02:31 +0200

    Fixed Vercel build error, hopefully?

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.SITE_URL || "https://www.devclub.blog/",
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
-  // ...other options
-};
-
-export default config;
